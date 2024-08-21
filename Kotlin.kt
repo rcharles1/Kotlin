18cbe8d4fe9e6da66d79c453dfeec4bcd39764cd
Robin Charles <robincharles940@gmail.com>
2024-08-21 09:55:05 +0300

    first commit

---
+++ Kotlin.kt
@@ -0,0 +1,12 @@
+/* 
+----------------------------------------Introduction to Kotlin ----------------------------------------------------------------------
+*/
+
+//  Data Types & Variables
+   1. mutable Variables (var)
+   2. immutable vatiables (val)
+
+   Syntax;
+        val/var variableName: dataType = value
+
+    Variable Inference
